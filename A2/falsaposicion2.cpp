0ec16e4426cc3f73bbf1ede0866c0033e9d376a4
iCharlieAraiza <eliptikman1234@gmail.com>
2020-09-27 18:48:37 -0500

    Create falsaposicion2.cpp

---
+++ A2/falsaposicion2.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <math.h>
+
+#define f(x)  (-3*x*x*x+5*x+1)
+
+using namespace std;
+
+double bisect( float x1, float xu, float es, int imax, float xr, int iter, int ea);
+
+int main()
+{
+    cout << bisect(-0.4,0.01,0.1,20,5,20,0.01);
+
+    return 0;
+}
+
+double bisect( float x1, float xu, float es, int imax, float xr, int iter, int ea){
+    iter = 0;
+    float fi = f(x1);
+    float fr;
+    float xrold;
+    float test;
+    
+    do{
+        xrold = xr;
+        xr = (x1 + xu) / 2;
+        fr = f(xr);
+       
+        iter = iter + 1;
+        
+        if(xr !=0){
+            ea = abs((xr-xrold)/xr) * 100;
+        }
+        test = fi*fr;
+    
+
+        if(test < 0){
+            xu = xr;
+        }else if(test > 0){
+            x1 = xr;
+            fi = fr;
+        }else{
+            ea = 0;
+        }
+    }while(ea >es || iter <= imax);
+    return xr;
+}
