a1ebffdd0fb5ed29f0a7a3dd6478541d1b64485e
iCharlieAraiza <eliptikman1234@gmail.com>
2020-09-28 03:38:33 -0500

    Create secantemodificado.cpp

---
+++ proyecto1/secantemodificado.cpp
@@ -0,0 +1,23 @@
+#include<iomanip>
+#include<math.h>
+#include<stdlib.h>
+
+using namespace std;
+
+#define f(x) (0.95*x*x*x)-5.9*x*x+(10.9*x-6)
+
+int main()
+{
+    float xi = 3.5, d = 0.01;
+    int n = 0;
+    
+	cout<<"Solución por método de la secante modificado"<< endl;
+	cout<<"==================================0"<< endl;
+    do{
+        xi = xi-( (f(xi))/f(xi+d)-f(xi) );
+        cout<< f(xi+d) <<endl;
+        n++;
+    }while(n < 3);
+
+    return 0;
+}
